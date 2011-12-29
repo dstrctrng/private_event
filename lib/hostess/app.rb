b934cf3a1e91dc88ac3f5cebba0693163938be8e
Tom Bombadil <amanibhavam@destructuring.org>
2011-12-28 22:53:19 -0800

    proxy to rubygems when files arent found

--- lib/hostess/app.rb
+++ lib/hostess/app.rb
@@ -1,10 +1,20 @@
+require 'open-uri'
+
 module Hostess 
   class App < Sinatra::Base
     enable :raise_errors
     disable :show_exceptions
 
     def serve
-      send_file(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'server', 'repo', request.path_info)))
+      fname = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'server', 'repo', request.path_info))
+      if File.exists? fname
+        puts "serving #{fname} from disk"
+        send_file fname
+      else
+        puts "couldnt find #{fname}"
+        puts "fetching #{request.path_info} from http://rubygems.org"
+        URI.parse("http://rubygems.org#{request.path_info}").read
+      end
     end
 
     %w[/specs.4.8.gz
